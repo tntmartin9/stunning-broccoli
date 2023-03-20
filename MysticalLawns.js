99b3d9a04a1c4ddaade1bf3089923891848b5edd
tntmartin9 <40325488+tntmartin9@users.noreply.github.com>
2023-03-19 22:25:27 -0500

    Update MysticalLawns.js

--- MysticalLawns.js
+++ MysticalLawns.js
@@ -1,15 +1,23 @@
-//let about = document.getElementById("about");
-
 function back() {
     console.log("step 2");
-    hideEverything();
-    document.getElementById("mainpng").style.height = '750px';
-    document.getElementById("src").style.top = '340%';
-    document.getElementById("bkcover1").style.display = 'inline';
-    document.getElementById("bkcover2").style.display = 'inline';
-    document.getElementById("MysticalLawns").style.display = 'inline';
+    hideEverything()
+    document.getElementById("mainpng").style.display = 'inline';
+    document.getElementById("ahhhhhhh").style.display = 'inline';
     document.getElementById("menu").style.display = 'inline';
+    document.getElementById("MysticalLawns").style.display = 'inline';
     console.log("step 3");
+}
+
+function backTwo() {
+    //setup
+    document.getElementById("SEPageJobs").style.display = 'none';
+    document.getElementById("SEPageMulching").style.display = 'none';
+    document.getElementById("SEPagePlanting").style.display = 'none';
+    document.getElementById("SEPageWeeding").style.display = 'none';
+    document.getElementById("SEPageCleaning").style.display = 'none';
+    document.getElementById("SEPageMowing").style.display = 'none';
+    //action
+    document.getElementById("SEPage").style.display = 'inline';
 }
 
 function openAbout() {
@@ -21,21 +29,42 @@ function openAbout() {
 
 function SE() {
     hideEverything();
-    document.getElementById("mainpng").style.height = '90%';
-    document.getElementById("src").style.top = '90%';
+    document.getElementById("SEPageBK").style.display = 'inline';
     document.getElementById("SEPage").style.display = 'inline';
 }
 
 function SEPageMowing() {
     hideEverything();
-    document.getElementById("SEPageMowing").style.display = 'inline';
     document.getElementById("SEPageJobs").style.display = 'inline';
+    document.getElementById("SEPageMowing").style.display = 'inline';
+}
+
+function SEPageMulching() {
+    hideEverything();
+    document.getElementById("SEPageJobs").style.display = 'inline';
+    document.getElementById("SEPageMulching").style.display = 'inline';
+}
+
+function SEPagePlanting() {
+    hideEverything();
+    document.getElementById("SEPageJobs").style.display = 'inline';
+    document.getElementById("SEPagePlanting").style.display = 'inline';
+}
+
+function SEPageWeeding() {
+    hideEverything();
+    document.getElementById("SEPageJobs").style.display = 'inline';
+    document.getElementById("SEPageWeeding").style.display = 'inline';
+}
+
+function SEPageCleaning() {
+    hideEverything();
+    document.getElementById("SEPageJobs").style.display = 'inline';
+    document.getElementById("SEPageCleaning").style.display = 'inline';
 }
 
 function openJoinUsPage() {
     hideEverything();
-    document.getElementById("mainpng").style.height = '100%';
-    document.getElementById("src").style.top = '100%';
     document.getElementById("joinUsPage").style.display = 'inline';  
 }
 
@@ -45,15 +74,24 @@ function openContactUsPage() {
 }
 
 function hideEverything() {
+    document.getElementById("mainpng").style.display = 'none';
     document.getElementById("menu").style.display = 'none';
-    document.getElementById("bkcover1").style.display = 'none';
-    document.getElementById("bkcover2").style.display = 'none';
+    document.getElementById("ahhhhhhh").style.display = 'none';
     document.getElementById("MysticalLawns").style.display = 'none';
     document.getElementById("aboutPage").style.display = 'none';
     document.getElementById("aboutPagepng").style.display = 'none';
+
+    //estimates page
     document.getElementById("SEPage").style.display = 'none';
     document.getElementById("SEPageMowing").style.display = 'none';
+    document.getElementById("SEPageMulching").style.display = 'none';
+    document.getElementById("SEPagePlanting").style.display = 'none';
+    document.getElementById("SEPageWeeding").style.display = 'none';
+    document.getElementById("SEPageCleaning").style.display = 'none';
+    document.getElementById("SEPageBK").style.display = 'none';
     document.getElementById("SEPageJobs").style.display = 'none';
+
+    //join and talk to us pages
     document.getElementById("joinUsPage").style.display = 'none';  
     document.getElementById("contactUsPage").style.display = 'none'; 
 }
@@ -68,7 +106,7 @@ function scheduleAppointment() {
     if (schedule_appointment === false) {
         schedule_appointment = true;
         console.log("1");
-        SEPSEO.textContent = "Sure, We will need your address and email address or phone number after you have clicked which job you would like us to do.";
+        SEPSEO.textContent = "Hallo";
 
     } else if (schedule_appointment === true) {
         schedule_appointment = false;
